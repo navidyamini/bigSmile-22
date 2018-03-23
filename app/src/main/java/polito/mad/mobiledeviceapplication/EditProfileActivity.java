64d71dec11ed5f01a373fac16d193033fef5bcd8
s.micalizzi <ser.gio92@hotmail.it>
2018-03-23 17:37:22 +0100

    creating edit activity

---
+++ app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
@@ -0,0 +1,27 @@
+package polito.mad.mobiledeviceapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    private static int PICK_IMAGE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_activity);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/polito/mad/mobiledeviceapplication/ShowProfileActivity.java
@@ -0,0 +1,19 @@
+package polito.mad.mobiledeviceapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class ShowProfileActivity extends AppCompatActivity {
+
+    private static int PICK_IMAGE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_activity);
+
+    }
+}
