08d76ca830e867cf20659ff047dcd99642fcf3a1
navidyamini <navidyamini@gmail.com>
2018-03-24 21:05:59 +0100

    creating save menu,

--- app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
+++ app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
@@ -1,12 +1,8 @@
 package polito.mad.mobiledeviceapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -21,7 +17,7 @@ public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu_save,menu);
         return true;
     }
 }
--- app/src/main/java/polito/mad/mobiledeviceapplication/ShowProfileActivity.java
+++ app/src/main/java/polito/mad/mobiledeviceapplication/ShowProfileActivity.java
@@ -3,6 +3,7 @@ package polito.mad.mobiledeviceapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,5 +16,11 @@ public class ShowProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_activity);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit,menu);
+        return true;
     }
 }
