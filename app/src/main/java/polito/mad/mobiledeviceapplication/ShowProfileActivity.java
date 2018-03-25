d35f58bdcc22eb355444e186d532ccc2ac598f91
navidyamini <navidyamini@gmail.com>
2018-03-25 13:45:22 +0200

    added Italian sting.xml, create the onclick event for edit button to go to the edit activity

--- app/src/main/java/polito/mad/mobiledeviceapplication/ShowProfileActivity.java
+++ app/src/main/java/polito/mad/mobiledeviceapplication/ShowProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +22,19 @@ public class ShowProfileActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_edit,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_edit:
+                Intent intent = new Intent(this, EditProfileActivity.class);
+                this.startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
         return true;
     }
 }
