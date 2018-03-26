d72be1fda8b14f305e5232865ce7ccb54fad8dbe
navidyamini <navidyamini@gmail.com>
2018-03-26 17:12:43 +0200

    writing code for saving profile in edit page

--- app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
+++ app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
@@ -1,19 +1,41 @@
 package polito.mad.mobiledeviceapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    private TextView nameText,emailText,phoneText,bioText, cityText, zipCodeText, zoneText;
+
     private static int PICK_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_activity);
 
+        nameText = (TextView)findViewById(R.id.name_edit);
+        emailText = (TextView)findViewById(R.id.email_edit);
+        phoneText = (TextView)findViewById(R.id.phone_edit);
+        bioText = (TextView)findViewById(R.id.bio_edit);
+        cityText = (TextView)findViewById(R.id.city_edit);
+        zipCodeText = (TextView)findViewById(R.id.zip_edit);
+        zoneText = (TextView)findViewById(R.id.zone_edit);
+
+        SharedPreferences settings = getSharedPreferences("ProfilePref",0);
+
+        nameText.setText(settings.getString("nameValue",""));
+        emailText.setText(settings.getString("emailValue",""));
+        phoneText.setText(settings.getString("phoneValue",""));
+        bioText.setText(settings.getString("bioValue",""));
+        cityText.setText(settings.getString("cityValue",""));
+        zipCodeText.setText(settings.getString("zipCodeValue",""));
+        zoneText.setText(settings.getString("zoneValue",""));
 
     }
 
@@ -34,5 +56,22 @@ public class EditProfileActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        SharedPreferences settings = getSharedPreferences("ProfilePref",0);
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("nameValue", nameText.getText().toString());
+        editor.putString("emailValue", emailText.getText().toString());
+        editor.putString("phoneValue", phoneText.getText().toString());
+        editor.putString("bioValue", bioText.getText().toString());
+        editor.putString("cityValue", cityText.getText().toString());
+        editor.putString("zipCodeValue", zipCodeText.getText().toString());
+        editor.putString("zoneValue", zoneText.getText().toString());
+        editor.commit();
+
     }
 }
