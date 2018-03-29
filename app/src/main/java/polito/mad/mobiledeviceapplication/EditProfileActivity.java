06c573d4620b217e5576dd83ba511c57ae48ea06
navidyamini <navidyamini@gmail.com>
2018-03-29 20:28:05 +0200

    changed the picture icon

--- app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
+++ app/src/main/java/polito/mad/mobiledeviceapplication/EditProfileActivity.java
@@ -2,17 +2,28 @@ package polito.mad.mobiledeviceapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditProfileActivity extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMAGE = 10;
     private EditText nameText,emailText,phoneText,bioText, cityText, zipCodeText, zoneText;
-
+    private ImageView profileImage;
     private static int PICK_IMAGE = 1;
 
     @Override
@@ -20,6 +31,14 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_activity);
 
+        profileImage = (ImageView) findViewById(R.id.image);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
         nameText = (EditText)findViewById(R.id.name_edit);
         emailText = (EditText)findViewById(R.id.email_edit);
         phoneText = (EditText)findViewById(R.id.phone_edit);
@@ -84,6 +103,31 @@ public class EditProfileActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK
+                && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
 
+            if (cursor == null || cursor.getCount() < 1) {
+                return; // no cursor or no record. DO YOUR ERROR HANDLING
+            }
 
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+
+            if(columnIndex < 0) // no column index
+                return; // DO YOUR ERROR HANDLING
+
+            String picturePath = cursor.getString(columnIndex);
+
+            cursor.close(); // close cursor
+            profileImage.setImageBitmap(BitmapFactory.decodeFile(picturePath.toString()));
+
+        }
+    }
 }
